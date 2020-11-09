fd6320d60a46e8ca519e2d142ed68ad2faa21b2d
bguilas <bryanjguilas@gmail.com>
2020-11-08 23:15:46 -0800

    front end added and syed create post added

---
+++ client/src/pages/common/components/CreatePost.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import Save from "./Save";
+import ButtonAppBar from "./ButtonAppBar";
+import '../style/CreatePost.css';
+
+class CreatePost extends Component {
+  state = {
+    title: "",
+    content: "",
+    image: "",
+    postSubmitted: false,
+  };
+
+  onChange = (input) => (e) => {
+    this.setState({
+      [input]: e.target.value,
+    });
+  };
+
+  submitPost = (e) => {
+    if (!this.state.title || !this.state.content) {
+      alert("All fields should be filed");
+      e.preventDefault();
+    } else {
+      this.setState({
+        postSubmitted: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <ButtonAppBar />
+        {!this.state.postSubmitted ? (
+          <div className="container">
+            <div className="jumbotron mt-3">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="well well-sm">
+                    <form className="form-horizontal" method="post">
+                      <fieldset>
+                        <legend className="text-center header" id="adp">
+                          Add new post
+                        </legend>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onChange("title")}
+                            name="title"
+                            type="text"
+                            placeholder="Post-Title"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onChange("image")}
+                            name="image"
+                            type="text"
+                            placeholder="image http://"
+                            className="form-control"
+                          ></input>
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-pencil-square-o bigicon"></i>
+                          </span>
+                          <textarea
+                            onChange={this.onChange("content")}
+                            name="content"
+                            type="text"
+                            placeholder="Your post"
+                            className="form-control"
+                            rows="7"
+                          ></textarea>
+                        </div>
+                        <div className="form-group">
+                          <button
+                            onClick={this.submitPost}
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Submit Post
+                          </button>
+                        </div>
+                      </fieldset>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+            <Save
+              title={this.state.title}
+              content={this.state.content}
+              image={this.state.image}
+            />
+          )}
+      </>
+    );
+  }
+}
+
+export default CreatePost;
