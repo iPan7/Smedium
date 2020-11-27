13fddf33f05cbe1fb6e4aa7ee75a3b87571b14a3
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-11-27 15:01:11 -0800

    added refactored route handler using res.sendFile

--- socketIoPlayground/index.js
+++ socketIoPlayground/index.js
@@ -2,6 +2,12 @@
 const app = require ('express')();
 const http = require ('http').createServer(app);
 
+//defined route handler using '/'
 app.get('/', (req, res) => {
+ res.sendFile('<h1>Hello World</h1>'); //calling res.send and passing string of HTML. refactored route handler
+});
 
-})
+//server listening on port 3000
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
