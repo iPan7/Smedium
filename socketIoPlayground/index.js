a1d4e08cf42f7dbf869df8cfc08f44f5b2f5ea55
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-11-27 15:50:31 -0800

    added user connected/disconnected functionality when refreshing page.

--- socketIoPlayground/index.js
+++ socketIoPlayground/index.js
@@ -1,11 +1,21 @@
 //initializes app
 const app = require ('express')();
 const http = require ('http').createServer(app);
+const io = require('socket.io') (http);
 
-//defined route handler using '/'
+//defined route handler using '/'. used GET as endpoint to connect to html
 app.get('/', (req, res) => {
- res.sendFile(__dirname + '/index.html') //calling res.send and passing string of HTML. refactored route handler
+ res.sendFile(__dirname + '/index.html'); //calling res.send and passing string of HTML. refactored route handler
 });
+
+//initializing instence of socket.io and passint http object and listen to connection event for incoming sockets.
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    //firing a new disconnect event/function
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    });
 
 //server listening on port 3000
 http.listen(3000, () => {
