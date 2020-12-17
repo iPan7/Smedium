867b47fffc96473b10d4132b0e0aa590cd474d7c
Ian Panlilio <ipanlilio7@gmail.com>
2020-12-16 19:14:41 -0800

    jawsdb url stringify

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.NODE_ENV === 'production') {
   connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
 } else {
   connection = mysql.createConnection({
-    host: 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'qc1548s7hzwui3z3',
-    password: 'crmg6n0rr04zep3s',
-    database: 'qtoqidubvpmff09f',
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'smedium_db',
   }).promise();
 }
 module.exports = connection;
