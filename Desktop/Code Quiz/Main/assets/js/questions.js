eb9492d65a6a5e88d8d4d8a9db31b48122cd6a9d
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-12-17 09:50:48 -0800

    added an addtional question to qeustions.js

--- Desktop/Code Quiz/Main/assets/js/questions.js
+++ Desktop/Code Quiz/Main/assets/js/questions.js
@@ -31,11 +31,13 @@ const questions = [
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
     answer: "console.log"
-  }
+  },
 
 {
   title:
-  "How to do you access "
+  "Accessing properties of an object is obtained by:",
+  choices: ["boolean", "plus sign", "period", "square brackets", "period or square brackets"],
+  answer: "period or square brackets"
 }
 
 ];
