e85c6121c914b6884ce8e0bdcc65a4731afc416f
Ian Panlilio <ipanlilio7@gmail.com>
2020-12-16 17:52:43 -0800

    added express server handler code to allow react router to take over paths not recognized by express

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use(cors());
