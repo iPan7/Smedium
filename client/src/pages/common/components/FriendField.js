1efc3c62aa1ce05ec1c79e57f2133c2da9bfd685
specilaist <specilaist@msn.com>
2020-12-19 14:22:37 -0800

    latest eban branch: still need to fix friend items

---
+++ client/src/pages/common/components/FriendField.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState, setState } from 'react';
+import API from '../../../utils/API';
+
+const FriendField = () => {
+	const [friends, setFriends] = useState('');
+
+	const getFriends = () => {
+		
+	}
+
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+export default FriendField;
--- model/friend/friendQueries.js
+++ model/friend/friendQueries.js
@@ -2,7 +2,7 @@ const findFriendsByUser = "SELECT friendId, friendName FROM friends WHERE friend
 
 const findFriendById = "SELECT * FROM friends WHERE id = ?;";
 
-const insertFriend = "INSERT INTO friends (friendId, content) VALUES (?, ?);";
+const insertFriend = "INSERT INTO friends (friendId) VALUES (?);";
 
 const deleteFriend = "DELETE * FROM friends WHERE id = ?;";
 
